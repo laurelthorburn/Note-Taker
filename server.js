f8a79d8dd9b53f038f530def558772bc9aa46540
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-02 18:20:46 -0600

    Creating dependencies

---
+++ server.js
@@ -0,0 +1,8 @@
+//dependencies
+const express = require('express');
+const path = require('path');
+const uuid = import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+
+const PORT = 3001;
+const app = express();
