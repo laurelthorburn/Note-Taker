0e9a2f975214bc5e912e784902ed6a8b9841dddc
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-02 18:36:57 -0600

    Creating dependencies

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //dependencies
-const express = require('express');
-const path = require('path');
-const uuid = import { v4 as uuidv4 } from 'uuid';
-uuidv4();
+const express = require('express'); //using express
+const fs = require('fs');
+const path = require('path'); //__dirname? -- might not need
+var uniqid = require('uniqid'); //creates unique ID - change to const?
 
 const PORT = 3001;
 const app = express();
+
+//psuedo code: need to store/retrieve 'fs'
