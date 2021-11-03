5c8ea857e5da8590bf7f6496bf493c50e12d7af7
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-02 18:45:00 -0600

    Added psuedo code in comments

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const PORT = 3001;
 const app = express();
 
 //psuedo code: need to store/retrieve 'fs'
+
+//Have two HTML Routes
+// GET /notes returns notes.html file
+//GET * returns index.html file
+
+// Create API routes:
+// GET /api/notes should read db.json file and return all saved notes as JSON
+// POST should receive new note to save on request body, add to db.json file, return new note to client (give each note unique ID using uniqid)
+
+//BONUS: DELETE
+// Add delete route to the app
+//DELETE /api/notes/:id = query parameter contains ID of note to delete.  Have to read all notes from db.json file and then remove note with given ID property and then rewrite notes to db.json file
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
