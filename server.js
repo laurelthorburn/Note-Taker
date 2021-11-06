58e5f2a8735370444581278db9d7b6e56ecabb8e
Laurel Thorburn <laurelthorburn@gmail.com>
2021-11-06 08:23:57 -0600

    Changed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express'); //using express
 const htmlRoute = require('./routes/htmlRoutes');
 const apiRoute = require('./routes/apiRoutes');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.port || 5000;
 
 const app = express();
 
